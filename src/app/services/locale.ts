8e9692282a7656df67f852bb739c648828853ea2
Flarion-ion <praestet.nobis@gmail.com>
2022-07-28 15:51:06 +0300

    welcome

---
+++ src/app/services/locale.ts
@@ -0,0 +1,17 @@
+import { Injectable} from "@angular/core";
+import {environment} from "../../environments/environment";
+import {Subject} from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isAuth = false;
+  isWelcome = new Subject<boolean>();
+  constructor() {
+    this.isAuth = environment.isAuthDefault;
+    this.isWelcome.next(false);
+  }
+
+}
