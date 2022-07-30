8c02f2233822e9470cb4a7711adf51cf2c7fc85d
Flarion-ion <praestet.nobis@gmail.com>
2022-07-30 15:12:37 +0300

    update docks

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -12,19 +12,40 @@ import {WorksComponent} from "../works/works.component";
 import {SkillsComponent} from "../skills/skills.component";
 import {ContactsComponent} from "../contacts/contacts.component";
 import {LocaleService} from "../services/locale";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {TimeoutService} from "../services/timeout";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
+  animations: [
+    trigger('start', [
+      // ...
+      state('begin', style({
+        width: '0%',
+      })),
+      state('end', style({
+        width: '100%',
+      })),
+      transition('begin => end', [
+        animate('0.5s')
+      ]),
+      transition('end => begin', [
+        animate('0.5s')
+      ]),
+    ]),
+  ]
 })
 export class MainPageComponent implements AfterViewInit {
   selected = 0
+  isVisible = false;
   @ViewChild('container', { read: ViewContainerRef })
   container!: ViewContainerRef;
   constructor(
     private factory: ComponentFactoryResolver,
-    private locale: LocaleService
+    private locale: LocaleService,
+    private delay: TimeoutService
   ) { }
 
   ngAfterViewInit(): void {
@@ -45,11 +66,14 @@ export class MainPageComponent implements AfterViewInit {
         break;
     }
     this.container.clear()
+    this.isVisible = false;
+    await this.delay.timeout(500);
+    this.isVisible = true;
+    await this.delay.timeout(400);
     this.container.createComponent(component);
   }
 
   select(id: number){
-    console.log(id)
     this.selected = id
     this.render()
   }
